04b36bff40dd953c6ccd822c388adeec01e37c7c
abhin5821 <abhinvaidya5821@gmail.com>
2024-08-20 20:59:16 +0530

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,1 +1,29 @@
+#Read the text file
 
+s=open("11.txt",'r')
+print(s.read())
+
+#read and write
+s=open("11.txt",'r+')
+print(s.read())
+
+#read a line
+s=open("11.txt",'r')
+print(s.readline())
+
+#Count the number of lines in text file
+
+s=open("11.txt",'r')
+s=len(readlines())
+print(s)
+
+
+
+
+
+
+
+
+  
+  
+    
